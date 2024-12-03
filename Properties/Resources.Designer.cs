b2b4cc78eca067f1573e18805be6121a05f2bbcd
LuisBessa01 <spooooks72@gmail.com>
2024-12-03 10:53:38 -0300

    MODIFY: nome do projeto

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Agenda_Telefonica.Properties {
+namespace Agenda-Luis.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Agenda_Telefonica.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Agenda_Telefonica.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Agenda-Luis.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
